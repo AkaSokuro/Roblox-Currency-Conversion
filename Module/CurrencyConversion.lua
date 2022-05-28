8318ada8f4c2a04cc4f28e470e272e509b197b6a
Aka <83358222+AkaSokuro@users.noreply.github.com>
2022-05-29 03:31:57 +0700

    Fix DefaultCurrency.

--- Module/CurrencyConversion.lua
+++ Module/CurrencyConversion.lua
@@ -7,13 +7,12 @@
 ]]
 
 
-
-local DefaultCurrency = "USD"
-
 local CurrencyRate = {
 	USD = 0.0125, -- 1 Robux equals to 1.25 cents in Dollar
 	DevEX = 0.0035 -- 1 Robux equals to 0.35 cents in Developer Exchange
 }
+
+local DefaultCurrency = CurrencyRate["USD"]
 
 
 
